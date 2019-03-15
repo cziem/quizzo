97c1e5d04ea8cd15b133dc2029a13faed92375c1
Fayvor George <phavorsparks@gmail.com>
2019-03-15 08:11:43 +0100

    initial app setup

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h2>{{ title }}</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      title: "Quizzo"
+    };
+  }
+};
+</script>
+
+<style scoped>
+header {
+  height: 12rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #35495e;
+  color: #9bb8d6;
+  font-size: 3.6rem;
+  border-bottom: 1px dotted #9bb8d6;
+}
+</style>
