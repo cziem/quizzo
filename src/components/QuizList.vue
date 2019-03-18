58a427e798d58397bf54d48c12d1675043fe9841
Fayvor George <phavorsparks@gmail.com>
2019-03-18 12:44:05 +0100

    paginate the questions

--- src/components/QuizList.vue
+++ src/components/QuizList.vue
@@ -1,8 +1,52 @@
 <template>
   <div class="quiz-list">
-    <ul>
-      <li :key="quiz.id" v-for="quiz in quizList">{{ quiz.question }}</li>
-    </ul>
+    <div class="tab" v-bind:class="{active: currentTab === 0}">
+      <h3>What is a framework?</h3>
+      <ul>
+        <li>An intelligent art</li>
+        <li>A framework is a work of frame</li>
+        <li>A work built on a frame</li>
+      </ul>
+    </div>
+    <div class="tab" v-bind:class="{active: currentTab === 1}">
+      <h3>Who is Arya Stark?</h3>
+      <ul>
+        <li>A girl has no name</li>
+        <li>The lady of Winterfell</li>
+        <li>A fullstack developer</li>
+      </ul>
+    </div>
+    <div class="tab" v-bind:class="{active: currentTab === 2}">
+      <h3>Coding is next to?</h3>
+      <ul>
+        <li>Rugby</li>
+        <li>Buisness</li>
+        <li>Skating</li>
+      </ul>
+    </div>
+    <div class="tab" v-bind:class="{active: currentTab === 3}">
+      <h3>A senior developer is known as?</h3>
+      <ul>
+        <li>Senior Dev</li>
+        <li>Senior citizen of a company or</li>
+        <li>Team Lead</li>
+      </ul>
+    </div>
+    <div class="tab" v-bind:class="{active: currentTab === 4}">
+      <h3>UI/UX is terminology for?</h3>
+      <ul>
+        <li>Sign Language</li>
+        <li>Mobile Applications</li>
+        <li>Extended Reality</li>
+      </ul>
+    </div>
+
+    <div style="overflow:auto;">
+      <div style="float:right;">
+        <button type="button" v-show="currentTab > 0" v-on:click="currentTab--">Previous</button>
+        <button type="button" v-on:click="nextQuiz" v-show="currentTab <= 3">Next</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +55,9 @@ export default {
   name: "QuizList",
   data() {
     return {
+      currentTab: 0,
+      tab: true,
+      active: true,
       quizList: [
         {
           question: "What is a framework?",
@@ -22,7 +69,7 @@ export default {
           question: "Who is Arya Stark?",
           id: 2,
           isCorrect: false,
-          answer: "A stack of cards"
+          answer: "A girl has no name"
         },
         {
           question: "Coding is next to?",
@@ -44,11 +91,33 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    nextQuiz: function(currentTab) {
+      if (this.currentTab >= 4) return;
+      this.currentTab++;
+    }
   }
 };
 </script>
 
 <style scoped>
+.tab {
+  display: none;
+}
+
+.tab.active {
+  display: block;
+}
+
+.tab h3 {
+  color: #d6ffe9;
+  font-size: 2em;
+  padding: 0.4em 0.5em;
+  border-left: 5px solid #237949;
+  background: #7ef0ce71;
+  margin-bottom: 0.2em;
+}
 .quiz-list ul {
   list-style-type: none;
 }
@@ -56,6 +125,21 @@ export default {
   background: rgb(142, 243, 186);
   padding: 1rem;
   margin-bottom: 0.5rem;
-  border-left: 5px solid #237949;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+}
+
+.quiz-list li:hover {
+  opacity: 0.7;
+  margin-left: 0.4em;
+}
+
+button {
+  border: none;
+  background: #35495e;
+  color: white;
+  padding: 0.5rem 2rem;
+  cursor: pointer;
+  margin-right: 0.5rem;
 }
 </style>
